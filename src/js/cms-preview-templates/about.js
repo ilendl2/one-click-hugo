00a105d3e82ba05d839b2463482566690c201b25
Lendl Smith <lendl.smith@gmail.com>
2019-09-10 09:43:52 -0400

    about page model

--- src/js/cms-preview-templates/about.js
+++ src/js/cms-preview-templates/about.js
@@ -1,18 +1,13 @@
 import React from "react";
 
-import Jumbotron from "./components/jumbotron";
-
 export default class AboutPreview extends React.Component {
   render() {
-    const {entry, getAsset, widgetFor} = this.props;
+    const {entry, widgetFor, getAsset} = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
 
-    if (image && !image.fileObj) {
-        image = window.parent.location.protocol + "//" + window.parent.location.host + image;
-    }
-
     return <div>
-      <Jumbotron image={image} title={entry.getIn(["data", "title"])}/>
+      <h2>{ entry.getIn(["data", "title"])}</h2>
+      { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
       { widgetFor("body") }
     </div>;
   }
